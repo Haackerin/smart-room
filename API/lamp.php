a0cb622cb1bb607619ab8d602a0707d6d60d42a0
Amirhossein <sefidgaramirhossein@gmail.com>
2023-01-27 15:31:29 +0330

    api complete

--- API/lamp.php
+++ API/lamp.php
@@ -1,14 +1,9 @@
 <?php
-
-
-$json = json_decode(file_get_contents('db.json'), true);
-$lamp = $json['lamp'];
-$door = $json['door'];
-
+$lamp = false;
+$door = false;
 
 if (isset($_GET['lamp']) && $_GET['lamp'] == 1) {
     $lamp = !$lamp;
 } elseif(isset($_GET['door']) && $_GET['door'] == 1){
     $door = !$door;
 }
-
